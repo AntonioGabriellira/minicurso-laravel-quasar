87df0874b9a80faf28d0cec85e356615b06bcc26
Gabriel <antoniogabrielsousalira2004@gmail.com>
2023-03-16 23:41:41 -0300

    first commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,79 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+
+        <q-toolbar-title>
+          APP de curriculos
+        </q-toolbar-title>
+
+        <div>
+          Quasar v{{ $q.version }}
+        </div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
+      <q-list>
+        <q-item-label header>
+          Menu principal
+        </q-item-label>
+
+        <EssentialLink v-for="link in essentialLinks" :key="link.title" v-bind="link"/>
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+import EssentialLink from 'components/EssentialLink.vue'
+
+const linksList = [
+  {
+    title: 'Home',
+    caption: 'Dashboard',
+    icon: 'mdi-home',
+    link:{name: 'home'},
+  },
+  {
+    title: 'Currículos',
+    caption: 'Listar currículos',
+    icon: 'mdi-text-box-outline',
+    link: {name: 'curriculo.listar'},
+  },
+ 
+]
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  components: {
+    EssentialLink
+  },
+
+  setup () {
+    const leftDrawerOpen = ref(false)
+
+    return {
+      essentialLinks: linksList,
+      leftDrawerOpen,
+      toggleLeftDrawer () {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      }
+    }
+  }
+})
+</script>
---
+++ src/pages/curriculos/ListarCurriculoPage.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="q-ma-lg"> 
+        <div class="text-h5"> 
+            Listar currículo 
+        </div>
+
+        <div class="q-my-lg">
+            <q-list bordered separator>
+                <q-item clickable v-ripple v-for="curriculo in curriculos" :key="curriculo.id">
+                    <q-item-section>
+                        <q-item-label>
+                            {{ curriculo.full_name }}
+                        </q-item-label>
+                        <q-item-label caption>
+                            {{ curriculo.email }}
+                        </q-item-label>
+                    </q-item-section>
+                </q-item>
+            </q-list>
+        </div>
+    </div>
+</template>
+
+<script>
+import {ref} from "vue";
+
+export default{
+    setup(){
+        const curriculos = ref([
+            { id: 1, 
+                full_name: 'Antonio Gabriel sousa lira',
+                first_name: 'A_gabriel', 
+                last_name: 'lira',
+                email:'ant@gmail.com',
+                confirmado: 0,
+            },
+            { id: 2, 
+                full_name: 'Romario marinho silva',
+                first_name: 'Romario', 
+                last_name: 'silva',
+                email:'silva@gmail.com',
+                confirmado: 0,
+            },
+            { id: 3, 
+                full_name: 'Silvana de soares',
+                first_name: 'Silvana', 
+                last_name: 'soares',
+                email:'soares@gmail.com',
+                confirmado: 0,
+            },
+        ]);
+
+        return {
+            curriculos,
+        };
+    },
+};
+</script>
+
+<style lang="css" scoped> </style>
